f8a515ea1faa5caf8fc21e6e27a9781d245c7674
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2022-01-27 00:50:11 +0530

    deletes dist folder

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-interface Props {
-    tags: Array<string>;
-    delimiter?: string;
-    onAdd: (arg: string) => void;
-    onDelete: (arg: number) => void;
-}
-declare const ReactFieldTag: ({ tags, delimiter, onAdd, onDelete }: Props) => JSX.Element;
-export default ReactFieldTag;
